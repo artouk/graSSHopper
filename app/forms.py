81a8096b5c5fe0080528a83374e2cb5a2746e96f
jjsearle <arto.seth@gmail.com>
2018-05-20 15:59:38 +0100

    Basic db schema added

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class NewHost(FlaskForm):
+    creator_id = IntegerField()
+    org_id = IntegerField()
+    host = StringField('Host', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    sysuser = StringField('System User')
+    port = IntegerField('Port', default=22)
+    submit = SubmitField('Save')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, NewHost
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Joe'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -16,13 +18,31 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=current_user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            #flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/host/new')
+@login_required
+def HostNew():
+    form = NewHost()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('newhost.html', title='New Host', form=form)
